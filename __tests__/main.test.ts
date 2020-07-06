d89f86acf01a245faebd409d3604f1762b701038
yutailang0119 <muta.yutaro@gmail.com>
2020-07-06 19:25:45 +0900

    Fix main.test target to Mackerel

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,22 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_SERVER_URL'] = 'https://api.mackerelio.com'
+  process.env['INPUT_VERSION'] = 'v0'
+  process.env['INPUT_PATH'] = 'org'
+  process.env['INPUT_HTTP_METHOD'] = 'GET'
+  process.env['INPUT_API_KEY'] = process.env.TEST_API_KEY
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
   }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
+
+  const orgName = process.env.TEST_ORG_NAME
+  expect(
+    cp.execSync(`node ${ip}`, options).toString()
+  ).toEqual(
+    `::set-output name=mackerel_api_response,::{"name":"${orgName}"}\n`
+  )
 })
